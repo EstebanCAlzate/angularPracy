a2067c7b050a4ab714df5d60bc70a0f41c6581d4
Esteban Alazate <esteban.alzate@babel.es>
2017-05-04 13:59:16 +0200

    Agenda. Dependencias

--- agenda/src/app/app.module.ts
+++ agenda/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {EjemplosComponentesComponent } from './ejemplos/ejemplos-componentes.com
 import { listaContactosComponent } from './lista-contactos/lista-contactos.component';
 import { ContactoService } from './servicios/contactos.service';
 import { DetallesContactoComponent } from './detalles-contacto/detalles-contacto.component';
+import { EjemplosInyeccionComponent, Servicio1, Servicio2, Servicio2Provider } from './ejemplos/ejemplos-inyeccion.component';
 
 
 interface Coche {
@@ -26,10 +27,13 @@ interface Coche {
     EjemplosBindingComponent,
     EjemplosComponentesComponent,
     listaContactosComponent,
-    DetallesContactoComponent
+    DetallesContactoComponent,
+    EjemplosInyeccionComponent
      ],
   providers: [ // metemos los servicios
-    ContactoService
+    ContactoService,
+    Servicio1, //tiene @injectable
+    Servicio2Provider
   ],
   bootstrap:    [ AppComponent ] //componente raiz de nyestra app
 })
---
+++ agenda/src/app/ejemplos/ejemplos-inyeccion.component.ts
@@ -0,0 +1,40 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+
+
+//inyeccion de dependencias usando decorador
+@Injectable()
+export class Servicio1 {
+    obtenerMensaje() { return 'Soy un servicio decorado con @injectable' };
+}
+
+//no es bonito hacer esto pero solo por ejemplo hacer mas class en un mismo .ts
+export class Servicio2 {
+    obtenerMensaje() { return 'Soy un servicio decorado que usa provedor de clase' };
+}
+
+//cuando llaaman a servicio2 usa la clase servicio2
+export const Servicio2Provider = {
+    provide: Servicio2,
+    useClass: Servicio2
+}
+
+@Component({
+    selector: 'ejemplos-inyeccion',
+    templateUrl: '/app/ejemplos/ejemplos-inyeccion.component.html'
+})
+
+export class EjemplosInyeccionComponent {
+
+    mensaje1: string;
+    mensaje2: string;
+
+    constructor(
+        private _servicio1: Servicio1,
+        private _servicio2: Servicio2
+    ) { }
+
+    OnInit() {
+        this.mensaje1 = this._servicio1.obtenerMensaje();
+        this.mensaje2 = this._servicio2.obtenerMensaje();
+    }
+}
